4a3e78a2a3de73cc3909bafb46300953194038d0
Leroy van Vliet <leroyvanvliet@gmail.com>
2022-10-07 00:52:02 +0200

    initialize project repo

---
+++ Pleione/mainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pleione
+//
+//  Created by Leroy van Vliet on 07/10/2022.
+//
+
+import UIKit
+
+class mainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
