558fa1c054a228b984d6a622b7a0685ffe864b2c
Leroy van Vliet <leroyvanvliet@gmail.com>
2022-10-21 12:25:46 -0700

    fix constraint bug

---
+++ Pleione/Data/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Pleione
+//
+//  Created by Leroy van Vliet on 10/10/2022.
+//
+
+import Foundation
---
+++ Pleione/Model/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Pleione
+//
+//  Created by Leroy van Vliet on 10/10/2022.
+//
+
+import Foundation
